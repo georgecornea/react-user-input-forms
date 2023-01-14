92dbe21052b06725026b428a07a545c561fbd6fa
George Cornea <george.cornea@outlook.com>
2023-01-14 14:15:58 -0800

    Updated SimpleInput to get user input using state with useState() and ref with useRef()

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,38 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [input, setInput] = useState('');
+  const inputRef = useRef();
+
+  const inputOnChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const inputOnFocusHandler = () => {
+    setInput('');
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    //console.log(input);
+    console.log(inputRef.current.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          value={input}
+          onChange={inputOnChangeHandler}
+          ref={inputRef}
+          onFocus={inputOnFocusHandler}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
