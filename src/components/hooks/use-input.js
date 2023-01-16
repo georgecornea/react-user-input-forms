0f4335469710bf2af7858d70662997c8d52dfd63
George Cornea <george.cornea@outlook.com>
2023-01-15 22:08:14 -0800

    Updated useInput() custom hook with useReducer()

--- src/components/hooks/use-input.js
+++ src/components/hooks/use-input.js
@@ -1,27 +1,70 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+// initial state for the reducer function
+const intitalState = {
+  inputValue: '',
+  isInputEdited: false,
+};
+
+// reducer takes previousState pass by React
+// and and and action pass by React, but need to use it in the code
+// return initially a default state, and then based on action dispached
+const inputStateReducer = (state, action) => {
+  if (action.type === 'INPUT') {
+    return { inputValue: action.value, isInputEdited: state.isInputEdited };
+  }
+
+  if (action.type === 'BLUR') {
+    return { isInputEdited: true, inputValue: state.inputValue };
+  }
+
+  if (action.type === 'RESET') {
+    return { isInputEdited: false, inputValue: '' };
+  }
+
+  //   return intitalState; - just used as dummy value
+};
 
 const useInput = (validateInput) => {
-  const [input, setInput] = useState('');
-  const [isInputEdited, setIsInputEdited] = useState(false);
+  // useReducer takes the the reducer funcion and the initial state
+  // returns an array with two elements
+  // manages state and a dispacher that allow us to dispach actions
+  const [inputState, dispach] = useReducer(inputStateReducer, intitalState);
 
-  const isInputValid = validateInput(input);
-  const error = !isInputValid && isInputEdited;
+  // don't required when we use useReducer()
+  //   const [input, setInput] = useState('');
+  //   const [isInputEdited, setIsInputEdited] = useState(false);
+
+  const isInputValid = validateInput(inputState.inputValue);
+  const error = !isInputValid && inputState.isInputEdited;
 
   const inputChangeHandler = (e) => {
-    setInput(e.target.value);
+    // don't required when we use useReducer()
+    // setInput(e.target.value);
+
+    // we pass action type and action value
+    dispach({ type: 'INPUT', value: e.target.value });
   };
 
   const inputBlurHandler = (e) => {
-    setIsInputEdited(true);
+    // don't required when we use useReducer()
+    // setIsInputEdited(true);
+
+    // we pass only action type
+    dispach({ type: 'BLUR' });
   };
 
   const resetInput = (e) => {
-    setInput('');
-    setIsInputEdited(false);
+    // don't required when we use useReducer()
+    //setInput('');
+    //setIsInputEdited(false);
+
+    // we pass only action type
+    dispach({ type: 'RESET' });
   };
 
   return {
-    input,
+    input: inputState.inputValue,
     error,
     inputChangeHandler,
     inputBlurHandler,
